a4f012687eb20eb34b8b46db8d3b1f5f1ca715d7
dimasquared <dimasquared@gmail.com>
2018-09-30 20:43:20 +0300

    solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let k = equation.match(/((- )|-)?\d+/g);
+  let a = +k[0];
+  let b = +k[2];
+  let c = +k[3];
+  let x1 = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
+  let x2 = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
+  let solutions = [x1, x2];
+  return solutions.sort((x1, x2) => x1 - x2);
 }
